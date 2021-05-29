1c3fcfbfe1b9f5c41e26ab84b4cc3e650a9ac661
Mefez <49673314+Mefez@users.noreply.github.com>
2021-05-29 20:29:34 +0300

    First_Test_Success
    I built the first unit test of our code and repaired a error detection class

--- test_errordetection.py
+++ test_errordetection.py
@@ -5,10 +5,13 @@ from unittest.mock import patch
 from unittest.mock import Mock
 import Model.errordetection
 import Model.irepo
+import Model.ierror
+import Model.iauthor
+import Model.icommit
 
 class TestED_OriginMasterBranchName_Test(unittest.TestCase):
 
-    def setUp(self):
+    """def setUp(self):
         class mockedbranch:
              def __init__(self, name):
                 self.name= name
@@ -33,7 +36,39 @@ class TestED_OriginMasterBranchName_Test(unittest.TestCase):
         
         b=Model.errordetection.ED_OriginMasterBranchName(Model.irepo.IRepo)
         
-        self.assertEqual(len(b.errorList), 0)
+        self.assertEqual(len(b.errorList), 0)"""
+    def setUp(self):
+
+        class mockedbranch:
+             def __init__(self, name):
+                self.name= name
+                self.headCommit = mockedcommit()
+
+             def comitter(self):
+                return ' '
+
+        class mockedcommit:
+             def __init__(self):
+                 self.committer= ' '
+                
+        class mockedbranches:
+             def __init__(self):
+                 self.branchList = []
+             def addBranch(self, name):
+                 branch = mockedbranch(name)
+                 self.branchList.append(branch)
+
+        self.irepo = mockedbranches()
+        self.irepo.addBranch("origin/origin/master")
+        self.irepo.addBranch("origin/pattern_change")
+
+    def test_ED_OriginMasterBranchName(self):
+        for e in self.irepo.branchList:
+            print(e.name)
+        
+        b=Model.errordetection.ED_OriginMasterBranchName(self.irepo)
+        
+        self.assertEqual(len(b.errorList), 1)
 
 if __name__ == '__main__':
     unittest.main()
