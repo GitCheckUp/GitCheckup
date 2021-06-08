2658ae9b5b10b5127910ca3db0ecbc03255fab14
Anil Guvenc <anilguvenc@gmail.com>
2021-06-09 02:18:23 +0300

    Chart slider added.
    Basic CSS slider is added for the charts in the result section.

--- GitCheckup/static/GitCheckup/js/main.js
+++ GitCheckup/static/GitCheckup/js/main.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
     $('.image-link').magnificPopup({type: 'image'});
     $('.video-link').magnificPopup({type: 'iframe'});
 });
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
