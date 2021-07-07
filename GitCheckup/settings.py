32fde8faa5ac7df46caf03dfdeeadfd2f395f5ca
Anil Guvenc <anilguvenc@gmail.com>
2021-07-07 19:54:30 +0300

    Update settings.py

--- GitCheckup/settings.py
+++ GitCheckup/settings.py
@@ -55,6 +55,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'GitCheckup.urls'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
